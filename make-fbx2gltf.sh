0b82c06114b69dbe414d12e51e4b484563e94067
Rayfoundry <daniel.z@rayfoundry.de>
2018-05-31 15:04:23 +0200

    Update make-fbx2gltf.sh

--- make-fbx2gltf.sh
+++ make-fbx2gltf.sh
@@ -2,6 +2,9 @@
 
 cp -R /root/fbxsdk/* /usr
 
+cd /root
+git clone https://github.com/facebookincubator/FBX2glTF.git
+
 cd /root/FBX2glTF
 cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Release
 make -Cbuild -j4 install
